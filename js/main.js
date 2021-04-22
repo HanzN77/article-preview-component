af72420512eb45ae5e2e29e78a0aca7eb0e57760
HanifNAfrian <64412921+JemiN77@users.noreply.github.com>
2021-04-22 21:40:42 +0700

    first design created

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+let medsos = document.querySelector(".share-link");
+let mobileVer = document.querySelector(".the-share-link");
+
+
+function data(){
+    
+    
+    
+    if(medsos.style.visibility == "hidden"){
+        medsos.style.visibility = "visible";
+    }else{
+        medsos.style.visibility = "hidden";
+
+    }
+
+
+    if(mobileVer.style.visibility == "hidden"){
+        mobileVer.style.visibility = "visible";
+    }else{
+        mobileVer.style.visibility = "hidden";
+    }
+}
